9392100e272b4642a1447931a7970bd9db576ee0
tracer-pa-public <tracer@njuics.org>
2022-06-11 22:05:30 +0800

    > clean > id icspa_mooc_student > user hutao1 > uname Linux debian11-pa 5.10.0-14-686-pae #1 SMP Debian 5.10.113-1 (2022-04-29) i686 GNU/Linux > uptime 22:05:30 up  8:45,  1 user,  load average: 0.28, 0.26, 0.20 > unitime > 145de962c794e6139a9dd2f6e481c8c6a0bf39cd

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -79,6 +79,7 @@ void add_PF(uint32_t res){
 		else{
 			cpu.eflags.PF = 0;
 		}
+		break;
 	default:
 		if(even == 1){
 			cpu.eflags.PF = 0;
@@ -86,6 +87,7 @@ void add_PF(uint32_t res){
 		else{
 			cpu.eflags.PF = 1;
 		}
+		break;
 	}
 }
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
