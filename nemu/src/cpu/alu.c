75ffa897f385037608547c3e03089e6fd0e258a9
tracer-pa-public <tracer@njuics.org>
2022-06-11 18:39:22 +0800

    > clean > id icspa_mooc_student > user hutao1 > uname Linux debian11-pa 5.10.0-14-686-pae #1 SMP Debian 5.10.113-1 (2022-04-29) i686 GNU/Linux > uptime 18:39:22 up  5:45,  1 user,  load average: 0.12, 0.06, 0.01 > unitime > d8f7a98b2cbaf440a7902c68cbd35400be9daeea

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -1,14 +1,92 @@
 #include "cpu/cpu.h"
+void add_SF(uint32_t res){
+	cpu.eflags.SF = sign(res);
+}
+void add_ZF(uint32_t res){
+	cpu.eflags.ZF = res == 0;
+}
 
+void add_OF(uint32_t res,uint32_t dest,uint32_t src,size_t data_size){
+	switch(data_size)
+	{
+		case 8:
+			res = sign_ext(res,data_size);
+			dest = sign_ext(dest,data_size);
+			src = sign_ext(src,data_size);
+			break;
+		case 16:
+			res = sign_ext(res,data_size);
+			dest = sign_ext(dest,data_size);
+			src = sign_ext(src,data_size);
+			break;
+		case 32:
+			res = sign_ext(res,data_size);
+			dest = sign_ext(dest,data_size);
+			src = sign_ext(src,data_size);
+			break;
+	}
+	if(sign(src) == sign(dest)){
+		if(sign(res) != sign(dest)){
+	 		cpu.elfags.OF = 1;
+		}
+		else{
+			cpu.eflags.OF = 0;	
+		}
+	}
+	else{
+		cpu.eflags.OF = 0;
+	}
+}
+
+void add_CF(uint32_t res,uint32_t src,size_t data_size){
+	res = sign_ext(res,data_size);
+	src = sign_ext(src,data_size);
+	cpu.eflags.CF = res < src;
+}
+void add_PF(uint32_t res){
+	int even = 0;
+	switch(res & 0xf)
+	{
+	case 0x0:
+	case 0x3:
+	case 0x6:
+	case 0xc:
+	case 0xf:
+ 		even = 1;//偶数
+		break;
+	default:
+		even = 0;//奇数
+		break;
+	switch((res >> 4) &0xf)
+	{
+	case 0x0:
+	case 0x3:
+	case 0x6:
+	case 0xc:
+	case 0xf:
+		if(even == 1){
+			cpu.elfags.PF = 1;
+		}
+		else{
+			cpu.eflags.PF = 0;
+		}
+	}
+}
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
 	return __ref_alu_add(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	uint32_t res = src +dest;
+        add_SF(res,data_size);
+	add_OF(res,dest,src,data_size);
+	add_ZF(res);	
+	add_PF(res);
+	add_CF(res,src,data_size);
+//	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
+//	fflush(stdout);
+//	assert(0);
+	return sign_ext(res,data_size);
 #endif
 }
 
