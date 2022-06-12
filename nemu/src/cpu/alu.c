bae4e69ea2d7d0c282609bc3475a199fad062db2
tracer-pa-public <tracer@njuics.org>
2022-06-12 21:25:57 +0800

    > clean > id icspa_mooc_student > user hutao1 > uname Linux debian11-pa 5.10.0-14-686-pae #1 SMP Debian 5.10.113-1 (2022-04-29) i686 GNU/Linux > uptime 21:25:57 up 15:42,  1 user,  load average: 0.21, 0.14, 0.10 > unitime > a249e81da7f98bcc876b4bc61ade42f79c1b70

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -171,7 +171,9 @@ void sb_OF(uint32_t res,uint32_t dest,uint32_t src,size_t data_size){
 		cpu.eflags.OF = 0;
 	}
 }
-void sb_CF(uint32_t dest,uint32_t src){
+void sb_CF(uint32_t dest,uint32_t src,size_t data_size){
+	dest = sign_ext(dest,data_size);
+	src = sign_ext(src,data_size);
 	cpu.eflags.CF = dest < src;
 }
 
@@ -184,7 +186,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 	all_SF(res,data_size);
 	all_ZF(res,data_size);	
 	all_PF(res);
-	sb_CF(dest,src);
+	sb_CF(dest,src,data_size);
 	sb_OF( res,dest,src,data_size);
 //	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 //	fflush(stdout);
