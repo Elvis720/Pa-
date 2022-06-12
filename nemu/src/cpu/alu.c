926d2a74f1d0810233d603c21c425a4a69558b87
tracer-pa-public <tracer@njuics.org>
2022-06-12 19:46:31 +0800

    > clean > id icspa_mooc_student > user hutao1 > uname Linux debian11-pa 5.10.0-14-686-pae #1 SMP Debian 5.10.113-1 (2022-04-29) i686 GNU/Linux > uptime 19:46:31 up 14:34,  1 user,  load average: 0.27, 0.23, 0.14 > unitime > 6585443889fab1e9bb3641f79ec5d8979c4fa282

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -186,7 +186,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 //	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 //	fflush(stdout);
 //	assert(0);
-	return res & (0xFFFFFFFF >> (32 - data_sieze));
+	return res & (0xFFFFFFFF >> (32 - data_size));
 #endif
 }
 
