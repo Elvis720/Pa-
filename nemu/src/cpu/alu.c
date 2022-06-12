68028377f697adeb9863d7850c0ee6c7b2de9903
tracer-pa-public <tracer@njuics.org>
2022-06-12 18:45:29 +0800

    > clean > id icspa_mooc_student > user hutao1 > uname Linux debian11-pa 5.10.0-14-686-pae #1 SMP Debian 5.10.113-1 (2022-04-29) i686 GNU/Linux > uptime 18:45:29 up 13:33,  1 user,  load average: 0.47, 0.22, 0.14 > unitime > 4805ee47fb8a0980bec5e8e3d253842e81099d2

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -118,7 +118,7 @@ void adc_CF(uint32_t res,uint32_t src,size_t data_size){
 		cpu.eflags.CF = res <= src;
 	}
 	else{
-		cpu.eflads.CF = res < src;
+		cpu.eflags.CF = res < src;
 	}
 }
 uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
