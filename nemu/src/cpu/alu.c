d0142fb00832fdb5eb4eea01b55458a92a07f30c
tracer-pa-public <tracer@njuics.org>
2022-06-13 09:38:24 +0800

    > clean > id icspa_mooc_student > user hutao1 > uname Linux debian11-pa 5.10.0-14-686-pae #1 SMP Debian 5.10.113-1 (2022-04-29) i686 GNU/Linux > uptime 09:38:24 up 17:41,  1 user,  load average: 0.44, 0.31, 0.21 > unitime > 369bfc9be6458136133ea8763496aab5cb69c8ad

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -303,10 +303,14 @@ uint32_t alu_and(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_and(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+//	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
+//	fflush(stdout);
+//	assert(0);
+	uint32_t res = dest | src;
+	all_PF(res);
+	all_ZF(res,data_size);
+	all_SF(res,data_size);
+	return res & (0xFFFFFFFF >>(32 - data_size));
 #endif
 }
 
