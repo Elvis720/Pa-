5e449b19634463f3b2674379838a035149fa4a8e
tracer-pa-public <tracer@njuics.org>
2022-06-15 11:05:35 +0800

    > clean > id icspa_mooc_student > user hutao1 > uname Linux debian11-pa 5.10.0-14-686-pae #1 SMP Debian 5.10.113-1 (2022-04-29) i686 GNU/Linux > uptime 11:05:35 up 12:08,  1 user,  load average: 0.19, 0.06, 0.03 > unitime > 180528255bc6a599788e7896ad226d2efad22b4

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -276,10 +276,15 @@ uint32_t alu_div(uint64_t src, uint64_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_div(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+//	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
+//	fflush(stdout);
+//	assert(0);
+	if(src == 0){
+		printf("遇到0了");
+		return;
+	}
+	uint64_t res = dest /src;
+	return res;
 #endif
 }
 
